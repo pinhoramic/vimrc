343a7bccca8df5204ae531148f6577238718b1ff
Ping-Hao Wu <pinghao@iswifter.net>
2011-11-08 10:15:28 -0800

    fix install

--- install.rb
+++ install.rb
@@ -21,9 +21,9 @@ if File.exist?(VIMRUNTIME)
   abort "#{VIMRUNTIME} already exists" unless Dir["#{VIMRUNTIME}/*"].empty?
 end
 
-system "git clone git://github.com/pinhoramic/vimrc.git #{$VIMHOME}"
+system "git clone git://github.com/pinhoramic/vimrc.git #{$VIMRUNTIME}"
 
-Dir.chdir VIMHOME
+Dir.chdir VIMRUNTIME
 system "git submodule update --init"
 
 Dir.mkdir "undodir"
